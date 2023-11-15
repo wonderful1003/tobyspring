f02c71e46f3f68591dc6c002630320fbfc5faf4e
BANG <BANG@192.168.219.108>
2023-11-15 23:33:05 +0900

    ㅇ

---
+++ test/src/test2/DaoFactory.java
@@ -0,0 +1,9 @@
+package test2;
+
+public class DaoFactory {
+	public UserDao userDao() {
+		ConnectionMaker connectionMaker = new DConnectionMaker();
+		UserDao userDao = new UserDao(connectionMaker);
+		return userDao;
+	}
+}
