fc09b108cf0e6152573722cf8f171113fd60bce8
BANG <BANG@192.168.219.108>
2024-07-07 19:44:15 +0900

    1.6.1 싱글톤 레지스트리로서의 애플리케이션 컨텍스트 - 싱글톤 패턴의 한계

--- src/tobyspring/UserDao.java
+++ src/tobyspring/UserDao.java
@@ -7,11 +7,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
+	private static UserDao INSTANCE;
 	
 	private ConnectionMaker connectionMaker;
 
-	public UserDao(ConnectionMaker connectionMaker) {
+	private UserDao(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
+	}
+	
+	public static synchronized UserDao getInstance() {
+		if(INSTANCE == null) INSTANCE = new UserDao(???);
+		return INSTANCE;
 	}
 
 	public void add(User user) throws ClassNotFoundException, SQLException{
