b516dde73049365ac418967630c2e6675f6e1ccd
BANG <BANG@192.168.219.108>
2024-07-07 16:39:43 +0900

    1.5.2 DaoFactory를 사용하는 애플리케이션 컨테스트 및 스프링라이브러리 추가

--- src/tobyspring/DaoFactory.java
+++ src/tobyspring/DaoFactory.java
@@ -1,21 +1,28 @@
 package tobyspring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+	
+	@Bean
 	public UserDao userDao() {
 //		ConnectionMaker connectionMaker = new DConnectionMaker();
 //		UserDao userDao = new UserDao(connectionMaker);
 		return new UserDao(connectionMaker());
 	}
 	
-	public AccountDao accountDao() {
-		return new accountDao(connectionMaker());
-	}
-
-	public MessageDao messageDao() {
-		return new MessageDao(connectionMaker());
-	}
+//	public AccountDao accountDao() {
+//		return new accountDao(connectionMaker());
+//	}
+//
+//	public MessageDao messageDao() {
+//		return new MessageDao(connectionMaker());
+//	}
 	
-	public connectionMaker connectionMaker() {
-		return new DconnectionMaker();
+	@Bean
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
 	}
 }
--- src/tobyspring/UserDaoTest.java
+++ src/tobyspring/UserDaoTest.java
@@ -1,13 +1,18 @@
 package tobyspring;
 
 import java.sql.SQLException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		
 //		ConnectionMaker connectionMaker = new DConnectionMaker();
-		
-		UserDao dao = new DaoFactory().userDao();
+//		UserDao dao = new DaoFactory().userDao();
+
+		ApplicationContext context = 
+				new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("wonderful");
