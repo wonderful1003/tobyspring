b8987526f595b07a14b0561eb69aee61a42620dc
eactive <eactive@eactive>
2024-07-08 16:43:10 +0900

    1.7.4 의존관계 주입의 응용 - 부가기능 추가

---
+++ src/tobyspring/CountingConnectionMaker.java
@@ -0,0 +1,25 @@
+package tobyspring;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker{
+
+	int counter = 0;
+	private ConnectionMaker realConnectionMaker; 
+	
+	public CountingConnectionMaker(ConnectionMaker reConnectionMaker) {
+		this.realConnectionMaker = reConnectionMaker;
+	}
+	
+	@Override
+	public Connection makeConnection() throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		this.counter++;
+		return realConnectionMaker.makeConnection();
+	}
+	
+	public int getCounter() {
+		return this.counter;
+	}
+}
---
+++ src/tobyspring/UserDaoConnectionCountingTest.java
@@ -0,0 +1,30 @@
+package tobyspring;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class UserDaoConnectionCountingTest {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		
+		ApplicationContext context = 
+				new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+	
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("wonderful");
+		user.setName("방승현");
+		user.setPassword("married");
+		
+		dao.add(user);
+		dao.add(user);
+		CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+		
+		System.out.println(" Connection counter : "+ccm.getCounter());
+		
+		
+	}
+}
