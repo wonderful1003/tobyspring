d8e10e604eb7aff95660731317facc378d289497
BANG <BANG@192.168.219.108>
2024-07-10 23:33:48 +0900

    2.2.2 테스트의 효율적인 수행과 결과 관리 - 검증 코드 전환

--- src/tobyspring/UserDaoTest.java
+++ src/tobyspring/UserDaoTest.java
@@ -2,9 +2,9 @@ package tobyspring;
 
 import java.sql.SQLException;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
@@ -12,10 +12,10 @@ public class UserDaoTest {
 	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
 		
-		ApplicationContext context = 
-				new GenericXmlApplicationContext("/**/applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
+		ApplicationContext context = new GenericXmlApplicationContext(
+				"/**/applicationContext.xml");
 		
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		User user = new User();
 		user.setId("wonderful");
 		user.setName("방승현");
@@ -23,16 +23,9 @@ public class UserDaoTest {
 		
 		dao.add(user);
 		
-		System.out.println(user.getId() + " success");
-		
 		User user2 = dao.get(user.getId());
 		
-		if (!user.getName().equals(user2.getName())) { 
-			System.out.println(" 테스트 실패 (name)"); 
-		}else if (!user.getPassword().equals(user2.getPassword())) { 
-			System.out.println(" 테스트 실패 (password)"); 
-		}	else { 
-			System.out.println("조회 테스트 성공");
-		}			
+		Assert.assertEquals(user.getName(), user2.getName());	
+		Assert.assertEquals(user.getName(), user2.getName());	
 	}
 }
