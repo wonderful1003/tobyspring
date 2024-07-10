ba75a0622f410b57dee24a322e56869be7939294
BANG <BANG@192.168.219.108>
2024-07-10 23:24:50 +0900

    2.2.2 테스트의 효율적인 수행과 결과 관리 - 테스트 메소드 전환

--- src/tobyspring/UserDaoTest.java
+++ src/tobyspring/UserDaoTest.java
@@ -1,15 +1,17 @@
 package tobyspring;
 
 import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+	
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
 		
-//		UserDao dao = new DaoFactory().userDao();
- 
 		ApplicationContext context = 
 				new GenericXmlApplicationContext("/**/applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
