f9dcc4b397c6c2524739e9e6eda84a775c6182a6
eactive <eactive@eactive>
2024-07-12 10:28:57 +0900

    2.3.3 포괄적인 테스트 - getCount() 테스트

--- src/tobyspring/UserDaoTest.java
+++ src/tobyspring/UserDaoTest.java
@@ -47,4 +47,28 @@ public class UserDaoTest {
 		assertThat(user4.getPassword() , is(user4.getPassword()));
 		
 	}
+	
+	@Test 
+	public void count() throws SQLException, ClassNotFoundException { 
+		ApplicationContext context = new GenericXmlApplicationContext(
+				"/**/applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao" , UserDao.class); 
+		User userl = new User("gyumee", "박성철", "springnol"); 
+		User user2 = new User("leegw700", "이길원", "springno2"); 
+		User user3 = new User("bumJin", "박범진", "springno3");
+				
+		dao.deleteAll();
+		
+		assertThat(dao.getCount() , is(0)); 
+
+		dao.add(userl);
+		assertThat(dao.getCount(), is(1)); 
+		
+		dao.add(user2);
+		assertThat(dao.getCount() , is(2)); 
+
+		dao.add(user3);
+		assertThat(dao.getCount() , is(3));
+	}
 }
