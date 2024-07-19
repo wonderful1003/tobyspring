c27585f38e48c362291ef7d2bdf3fe3641d18583
eactive <eactive@eactive>
2024-07-19 15:16:02 +0900

    3.3.1 전략 클래스의 추가 정보

--- src/user/dao/UserDao.java
+++ src/user/dao/UserDao.java
@@ -21,19 +21,8 @@ public class UserDao {
 	}
 
 	public void add(User user) throws ClassNotFoundException, SQLException{
-
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("insert into user(id, name, password) values(?,?,?)");
-		
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
-		
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
+		StatementStrategy st = new AddStatement(user); 
+		jdbcContextWithStatementStrategy(st); 
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException{
