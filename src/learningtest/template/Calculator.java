35daf94c503af6c7015b60e5492b3a71f46ce6ed
BANG <BANG@192.168.219.107>
2024-08-07 23:42:21 +0900

    3.5.3 템플릿/콜백의 응용 - 중복의 제거와 템플릿/콜백 설계

--- src/learningtest/template/Calculator.java
+++ src/learningtest/template/Calculator.java
@@ -4,20 +4,46 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.springframework.aop.AfterReturningAdvice;
+
 public class Calculator {
 	public Integer calcSum(String filepath)throws IOException {
-		BufferedReader br = null;
+		BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+				Integer sum = 0;
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					sum += Integer.valueOf(line);
+				}
+				return sum;
+			}
+		};
+		return fileReadTemplate(filepath, sumCallback);
+	}
+	
+	public Integer calMultiply(String filepath)throws IOException {
 		
+		BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+				
+				Integer multiply = 1;
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					multiply *= Integer.valueOf(line);
+				}
+				return multiply;
+			}
+		};
+		return fileReadTemplate(filepath, multiplyCallback);
+	}
+	
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(filepath));
-			Integer sum = 0;
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				sum += Integer.valueOf(line);
-			}
-			return sum;
-			
-		}catch (Exception e) {
+			int ret = callback.doSomethingWithReader(br);
+			return ret;
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			throw e;
 		}finally {
@@ -27,9 +53,6 @@ public class Calculator {
 					System.out.println(e.getMessage());
 				}
 			}
-			
-			
 		}
-		
 	}
 }
