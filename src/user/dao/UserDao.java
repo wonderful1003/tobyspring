0518b391d92f742d4549c0fe3e3c6ed385ea284c
BANG <BANG@192.168.219.107>
2024-08-13 22:49:32 +0900

    3.6.2 queryForInt() - Deprecated queryForObject로 대체

--- src/user/dao/UserDao.java
+++ src/user/dao/UserDao.java
@@ -8,9 +8,11 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.junit.runner.JUnitCore;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import user.domain.User;
 
@@ -81,41 +83,7 @@ public class UserDao {
 	}
 	
 	public int getCount() throws SQLException{
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		
-		try {
-			c = dataSource.getConnection();
-		
-			ps = c.prepareStatement("select count(*) from user");
-			
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		}finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-				
-			}
-		}
+		return this.jdbcTemplate.queryForObject("select count(*) from user", Integer.class);
 	}
 	
 //	public static void main(String[] args) throws ClassNotFoundException, SQLException{
