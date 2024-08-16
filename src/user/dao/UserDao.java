c02d04ec024f80222230224f065ac59a100846c3
BANG <BANG@192.168.219.107>
2024-08-17 01:24:14 +0900

    3.6.5 재사용 가능한 콜백의 분리

--- src/user/dao/UserDao.java
+++ src/user/dao/UserDao.java
@@ -1,38 +1,35 @@
 package user.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.junit.runner.JUnitCore;
-import org.springframework.aop.ThrowsAdvice;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 import user.domain.User;
 
 public class UserDao {
 	
-	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
-		this.dataSource = dataSource;
 	}
-	
-	
-	public void setJdbcContext(JdbcContext jdbcContext) {
-	}	
 
+	private RowMapper<User> userMapper =  
+		new RowMapper<User>() {
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException{ 
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user; 
+			}
+		};
+	
 	public void add(final User user) throws ClassNotFoundException, SQLException{
 		this.jdbcTemplate.update("insert into user(id, name, password) values(?,?,?)",
 				user.getId(),user.getName(),user.getPassword());
@@ -44,51 +41,15 @@ public class UserDao {
 	
 	public User get(String id) throws ClassNotFoundException, SQLException{
 		return this.jdbcTemplate.queryForObject("select * from user where id =?",
-			new Object[] {id},
-			new RowMapper<User>() {
-				public User mapRow(ResultSet rs, int rowNum) 
-						throws SQLException{
-					User user = new User();
-					user.setId(rs.getString("id"));
-					user.setName(rs.getString("name"));
-					user.setPassword(rs.getString("password"));
-					return user;
-				}
-		});
+			new Object[] {id}, this.userMapper);
 	}
 
-	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
-		Connection c = null;
-		PreparedStatement ps = null;
-		
-		try {
-			c = dataSource.getConnection();
-			ps = stmt.makePreparedStatement(c); 
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		}finally {
-			if (ps != null){ try {ps.close(); } catch (SQLException e) {}}
-			if (c != null) { try {c.close(); } catch (SQLException e) {}}
-		}
-	}
-	
 	public int getCount() throws SQLException{
 		return this.jdbcTemplate.queryForObject("select count(*) from user", Integer.class);
 	}
 	
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from user order by id",
-			new RowMapper<User>() {
-				public User mapRow(ResultSet rs, int rowNum) 
-						throws SQLException{
-					User user = new User();
-					user.setId(rs.getString("id"));
-					user.setName(rs.getString("name"));
-					user.setPassword(rs.getString("password"));
-					return user; 
-				}
-		});
+		return this.jdbcTemplate.query("select * from user order by id", this.userMapper);
 	}
 	
 //	public static void main(String[] args) throws ClassNotFoundException, SQLException{
