ccb7366cd5d2afa162b2e4ce9b80cfe1ff1b772a
BANG <BANG@192.168.219.107>
2024-08-21 23:37:47 +0900

    5.1.1 필드 추가

--- src/user/UserDaoTest.java
+++ src/user/UserDaoTest.java
@@ -21,6 +21,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import user.dao.UserDaoJdbc;
+import user.domain.Level;
 import user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -40,12 +41,12 @@ public class UserDaoTest {
 	
 	@Before
 	public void setUp() {
-		this.user1 = new User("gyumee", "박성철", "springnol"); 
-		this.user2 = new User("leegw700", "이길원", "springno2"); 
-		this.user3 = new User("bumJin", "박범진", "springno3");
+		this.user1 = new User("gyumee", "박성철", "springnol", Level.BASIC, 1, 0); 
+		this.user2 = new User("leegw700", "이길원", "springno2", Level.SILVER, 55, 10); 
+		this.user3 = new User("bumJin", "박범진", "springno3", Level.GOLD, 100, 40);
 	}
 	
-	//@Test
+	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
 		
 		dao.deleteAll(); 
@@ -56,12 +57,10 @@ public class UserDaoTest {
 		assertThat(dao.getCount(), is(2));
 
 		User userget1 = dao.get(user1.getId()); 
-		assertThat(userget1.getName(), is(user1.getName())); 
-		assertThat(userget1.getPassword(), is(user1.getPassword())); 
-
+		checkSameUser(userget1, user1);
+		
 		User userget2 = dao.get(user2.getId()); 
-		assertThat(userget2.getName(), is(user2.getName())); 
-		assertThat(userget2.getPassword(), is(user2.getPassword()));
+		checkSameUser(userget2, user2);
 		
 	}
 	
@@ -95,7 +94,7 @@ public class UserDaoTest {
 		dao.get("unknown_id");
 	}
 	
-	@Test
+	//@Test
 	public void getAll() throws ClassNotFoundException, SQLException {
 		dao.deleteAll();
 		
@@ -131,5 +130,8 @@ public class UserDaoTest {
 		assertThat(user1.getId()  , is(user2.getId()));
 		assertThat(user1.getName(), is(user2.getName()));
 		assertThat(user1.getPassword()  , is(user2.getPassword()));
+		assertThat(user1.getLevel()  , is(user2.getLevel()));
+		assertThat(user1.getLogin()  , is(user2.getLogin()));
+		assertThat(user1.getRecommend()  , is(user2.getRecommend()));
 	}
 }
