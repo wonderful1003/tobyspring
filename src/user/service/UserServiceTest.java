a60b06deacefefbc8b7cda31e4f77673f8bd2b6a
BANG <BANG@192.168.219.107>
2024-09-18 23:44:01 +0900

    6.2.4 목 프레임워크 - Mockito 프레임워크 (라이브러리 버전문제로 테스트는 못해봄)

--- src/user/service/UserServiceTest.java
+++ src/user/service/UserServiceTest.java
@@ -3,6 +3,11 @@ package user.service;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -14,6 +19,7 @@ import javax.sql.DataSource;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
@@ -161,6 +167,34 @@ public class UserServiceTest {
 		}
 		
 		checkLevelUpgraded(users.get(1), false);
+	}
+	
+	@Test
+	public void mockUpgradeLevels() throws Exception {
+		UserServiceImpl userServiceImpl = new UserServiceImpl();
+		
+		UserDao mockUserDao = mock(UserDao.class);
+		when(mockUserDao.getAll()).thenReturn(this.users);
+		userServiceImpl.setUserDao(mockUserDao);
+		
+		MailSender mockMailSender = mock(MailSender.class);
+		userServiceImpl.setMailSender(mockMailSender);
+		
+		userServiceImpl.upgradeLevels();
+		
+		verify(mockUserDao, times(2)).update(any(User.class)); 
+		verify(mockUserDao, times(2)).update(any(User.class));
+		verify(mockUserDao).update(users.get(1)); 
+		assertThat(users.get(3).getLevel(), is(Level.GOLD));
+		
+		ArgumentCaptor<SimpleMailMessage> mailMessageArg = 
+				ArgumentCaptor.forClass(SimpleMailMessage.class); 
+		
+		verify(mockMailSender, times(2)).send(mailMessageArg.capture()); 
+		List<SimpleMailMessage> mailMessages = mailMessageArg.getAllValues(); 
+		assertThat(mailMessages.get(0).getTo()[0], is(users.get(1).getEmail())); 
+		assertThat(mailMessages.get(1).getTo()[0], is(users.get(3).getEmail())) ;
+		
 	}
 	
 	static class MockMailSender implements MailSender{
