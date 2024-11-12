d0b0759763cebfcb039a04f6f330a09b79a5fe2c
eactive <eactive@eactive>
2024-11-12 10:59:38 +0900

    6.5.3 포인트컷 표현식을 이용한 포인트컷 -  포인트컷 표현식 문법

--- src/learningtest/spring/pointcut/Target.java
+++ src/learningtest/spring/pointcut/Target.java
@@ -1,9 +1,39 @@
 package learningtest.spring.pointcut;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
 public class Target implements TargetInterface{
 	public void hello() {}
 	public void hello(String a) {}
 	public int minus(int a, int b) throws RuntimeException {return 0;}
 	public int plus(int a, int b) {return 0;}
 	public void method() {}
+	
+	@Test
+	public void methodSignaturePointcut() throws SecurityException, NoSuchMethodException{
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		
+		pointcut.setExpression("execution(public int " + 
+				"learningtest.spring.pointcut.Target.minus(int,int) "+
+				"throws java.lang.RuntimeException)");
+		
+		//Target.minus()
+		assertThat(pointcut.getClassFilter().matches(Target.class) && 
+				pointcut.getMethodMatcher().matches(
+						Target.class.getMethod("minus", int.class, int.class), null), is(true));
+		
+		//Target.plus()
+		assertThat(pointcut.getClassFilter().matches(Target.class) && 
+				pointcut.getMethodMatcher().matches(
+						Target.class.getMethod("plus", int.class, int.class), null), is(false));
+		
+		//Bean.method()
+		assertThat(pointcut.getClassFilter().matches(Bean.class) && 
+				pointcut.getMethodMatcher().matches(
+						Target.class.getMethod("method"), null), is(false));
+	}
 }
