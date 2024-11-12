c9d5ca7f5c2746cbb034bc025199a96b031ddc72
eactive <eactive@eactive>
2024-11-12 13:46:21 +0900

    6.5.3 포인트컷 표현식을 이용한 포인트컷 - 포인트컷 표현식 테스트

--- src/learningtest/spring/pointcut/Target.java
+++ src/learningtest/spring/pointcut/Target.java
@@ -36,4 +36,30 @@ public class Target implements TargetInterface{
 				pointcut.getMethodMatcher().matches(
 						Target.class.getMethod("method"), null), is(false));
 	}
+	
+	public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, 
+			String methodName, Class<?>... args) throws Exception{
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression(expression);
+		
+		assertThat(pointcut.getClassFilter().matches(clazz)
+				&& pointcut.getMethodMatcher().matches(
+						clazz.getMethod(methodName, args), null), is(expected));
+		
+	}
+	
+	public void targetClassPointcutMathces(String expression, boolean... expected) throws Exception{
+		pointcutMatches(expression, expected[0], Target.class, "hello");
+		pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+		pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+		pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+		pointcutMatches(expression, expected[4], Target.class, "method");
+		pointcutMatches(expression, expected[5], Bean.class, "method");
+	}
+	
+	@Test
+	public void pointcut() throws Exception{
+		targetClassPointcutMathces("execution(* *(..))", true,true,true,true,true,true);
+
+	}
 }
