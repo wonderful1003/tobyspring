75290eaf487aaa26e0f7e92152221a20475cccae
eactive <eactive@eactive>
2024-11-15 16:52:25 +0900

    6.7.2 트랜잭션 애노테이션 적용

--- src/user/service/UserService.java
+++ src/user/service/UserService.java
@@ -3,16 +3,22 @@ package user.service;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import user.domain.User;
 
+@Transactional
 public interface UserService {
 	void add(User user) throws ClassNotFoundException, SQLException;
-	
-	User get(String id) throws ClassNotFoundException, SQLException;;;
-	List<User> getAll();
 	void deleteAll() throws ClassNotFoundException, SQLException;;
 	void update(User user);
-	
 	void upgradeLevels();
+	
+	@Transactional(readOnly=true)
+	User get(String id) throws ClassNotFoundException, SQLException;
+	
+	@Transactional(readOnly=true)
+	List<User> getAll();
+	
 	
 }
