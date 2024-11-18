861c426d14d9a6112b34e0ff0f99cbf4a2bb966d
eactive <eactive@eactive>
2024-11-18 13:13:34 +0900

    6.8.2 트랜잭션 동기화와 테스트 - 트랜잭션 동기화 검증

--- src/user/service/UserServiceTest.java
+++ src/user/service/UserServiceTest.java
@@ -139,14 +139,19 @@ public class UserServiceTest {
 	
 	@Test 
 	public void transactionSync() throws ClassNotFoundException, SQLException {
+		userDao.deleteAll();
+		assertThat(userDao.getCount(), is(0));
+		
 		DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();
 		TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);
 		
-		userService.deleteAll(); 
 		userService.add(users.get(0)); 
 		userService.add(users.get(1));
+		assertThat(userDao.getCount(), is(2));
 		
-		transactionManager.commit(txStatus);
+		transactionManager.rollback(txStatus);
+		assertThat(userDao.getCount(), is(0));
+		
 	}
 
 	static class TestUserServiceImpl extends UserServiceImpl{
