3dbd84c7d66542ff87b480052761b6249fcad453
eactive <eactive@eactive>
2024-11-26 15:14:14 +0900

    7.2.6 디폴트 의존관계 - 확장 가능한 기반 클래스

---
+++ src/springbook/user/sqlservice/BaseSqlService.java
@@ -0,0 +1,29 @@
+package springbook.user.sqlservice;
+
+import javax.annotation.PostConstruct;
+
+public class BaseSqlService implements SqlService{
+	protected SqlReader sqlReader;
+	protected SqlRegistry sqlRegistry;
+	
+	public void setSqlReader(SqlReader sqlReader) {
+		this.sqlReader = sqlReader;
+	}
+
+	public void setSqlRegistry(SqlRegistry sqlRegistry) {
+		this.sqlRegistry = sqlRegistry;
+	}
+	
+	@PostConstruct
+	public void loadSql() {
+		this.sqlReader.read(this.sqlRegistry);
+	}
+
+	public String getSql(String key) throws SqlRetrievalFailureException {
+		try {
+			return this.sqlRegistry.findSql(key);
+		} catch (Exception e) {
+			throw new SqlRetrievalFailureException(e.getMessage());
+		}
+	}
+}
