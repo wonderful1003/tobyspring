7554360e0fec32f03bf41ff415e6e60781388b9c
BANG <BANG@192.168.219.108>
2024-12-28 22:21:19 +0900

    7.5.3 트랜잭션 적용 - 다중 SQL 수정에 대한 트랜잭션 테스트

--- src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -1,11 +1,17 @@
 package springbook.user.sqlservice.updatable;
 
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.After;
+import org.junit.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
+import springbook.user.sqlservice.SqlUpdateFailureException;
 import springbook.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
@@ -27,5 +33,22 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
 	@After
 	public void tearDown() {
 		db.shutdown();
+	}
+	
+	@Test
+	public void transactionalUpdate() {
+		checkFind("SQL1", "SQL2", "SQL3");
+		
+		Map<String, String> sqlmap = new HashMap<String, String>();
+		sqlmap.put("KEY1", "Modified1");
+		sqlmap.put("KEY9999!@#$", "Modified9999");
+		
+		try {
+			sqlRegistry.updateSql(sqlmap);
+			fail();
+		}
+		catch(SqlUpdateFailureException e) {}
+		
+		checkFind("SQL1", "SQL2", "SQL3");
 	}
 }
